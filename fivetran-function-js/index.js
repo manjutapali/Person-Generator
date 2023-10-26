fa09fcba3c993574d4d14e9fb421989b51167b1e
Manjunath Tapali <manjunath@fivetran.com>
2023-10-26 17:44:49 +0530

    added working code

--- fivetran-function-js/index.js
+++ fivetran-function-js/index.js
@@ -1,81 +1,38 @@
-//@ts-check
+exports.handler = (request, context, callback) => {
+    callback(null, main(request.state, request.secrets));
+};
 
-const https = require('https');
-
-/**
- * @param {!Object} req Cloud Function request context.
- * @param {!Object} res Cloud Function response context.
- */
-exports.handler = (req, res) => {
-    // Get a bearer token from twitter
-    console.log('Starting the handler execution')
-
-    console.log('Getting incoming parameters ...')
-    console.log('1.Consumer Key = ' + req.body.secrets.consumerKey)
-    console.log('1.Consumer Secret = ' + req.body.secrets.consumerSecret)
-
-    let post = https.request({
-        method: 'GET',
-        hostname: 'api.tfl.gov.uk',
-        path: '/line/mode/tube/status',
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    }, postRes => {
-        var reply = ''
-        postRes.setEncoding('utf8')
-        postRes.on('data', chunk => reply += chunk)
-        postRes.on('end', () => withReponse(JSON.parse(reply)))
-    })
-    
-    post.on('error', e => console.log(`Problem with request ${e}`))
-    post.write('grant_type=client_credentials')
-    post.end()
-    
-    // Get the TFL reponse and collect the data
-    function withReponse(timeline) {
-        console.log(`...got ${timeline.length} line status data, sending to Fivetran.`)
-
-        let since_id = null
-
-        // Reformat Twitter's response into nice, flat tables
-        let tflLineStatus = []
-        for (let t of timeline) {
-            // Remember the first id we encounter, which is the most recent
-            if (since_id == null) {
-                since_id = t.id
+function main(state, secrets) {
+    // Fetch records using api calls
+    let [insertTransactions, deleteTransactions, newTransactionsCursor] = apiResponse(state, secrets);
+    // Populate records and state
+    return ({
+        state: {
+            transactionsCursor: newTransactionsCursor
+        },
+        insert: {
+            transactions: insertTransactions
+        },
+        delete: {
+            transactions: deleteTransactions
+        },
+        schema : {
+            transactions : {
+                primary_key : ['order_id', 'date']
             }
+        },
+        hasMore : false
+    });
+}
 
-            console.log(`Selecting the schema values`);
-
-            // Add all tflLineStatus
-            tflLineStatus.push({
-                linename: t.id,
-                linestatus: t.lineStatuses[0].statusSeverityDescription,
-                timestamp : t.created
-            })
-        }
-        // Send JSON response back to Fivetran
-        res.header("Content-Type", "application/json")
-        res.status(200).send({
-            // Remember the most recent id, so our requests are incremental
-            state: {
-                since_id: since_id == null ? req.state.since_id : since_id
-            },
-            // Fivetran will use these primary keys to perform a `merge` operation,
-            // so even if we send the same row twice, we'll only get one copy in the warehouse
-            schema : {
-                tflLineStatus : {
-                    primary_key : ['linename']
-                }
-            },
-            // Insert these rows into my warehouse
-            insert: {
-                tflLineStatus: tflLineStatus
-            },
-            // If this is true, Fivetran will immediately call this function back for more data
-            // This is useful to page through large collections
-            hasMore : false
-        })
-    }
+function apiResponse(state, secrets) {
+    var insertTransactions = [
+            {"date":'2017-12-31T05:12:05Z', "order_id":1001, "amount":'$1200', "discount":'$12'},
+            {"date":'2017-12-31T06:12:04Z', "order_id":1001, "amount":'$1200', "discount":'$12'},
+    ];
+    var deleteTransactions = [
+            {"date":'2017-12-31T05:12:05Z', "order_id":1000, "amount":'$1200', "discount":'$12'},
+            {"date":'2017-12-31T06:12:04Z', "order_id":1000, "amount":'$1200', "discount":'$12'},
+    ];
+    return [insertTransactions, deleteTransactions, '2018-01-01T00:00:00Z'];
 }
